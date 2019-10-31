88380529e192ed6448d29abf731f4b881b8c6020
Shorshi <jorgejocampo@gmail.com>
2019-10-31 00:57:00 -0300

    eslint

--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './CharacterInfo.css';
 
-export default (props) => <div className="character-info">
+export default (props) => (
+  <div className="character-info">
     <div className="name">
       <h2>{props.name}</h2>
     </div>
     <div className="description">
       <div>
-        {props.class} Lv{props.level}
+        {props.class}
+        {' '}
+Lv
+        {props.level}
       </div>
       <div>
-        {props.exp} exp
+        {props.exp}
+        {' '}
+exp
       </div>
     </div>
-</div>
+  </div>
+);
 
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Stats.css';
 
-export default (props) => <div className="stats-container">
-  {props.stats.map((stat) => <div className="stat">
-    <div>
-      {stat.value}
-    </div>
-    <div className="stat-name">
-      {stat.name}
-    </div>
-  </div>)}
-</div>
+export default (props) => (
+  <div className="stats-container">
+    {props.stats.map((stat) => (
+      <div className="stat">
+        <div>
+          {stat.value}
+        </div>
+        <div className="stat-name">
+          {stat.name}
+        </div>
+      </div>
+    ))}
+  </div>
+);
